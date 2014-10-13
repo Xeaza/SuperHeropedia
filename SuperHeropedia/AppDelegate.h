f2ca9714e73c3b3942eb279e54ec719b131470aa
xeaza <taylorwrightsanson@gmail.com>
2014-10-13 10:50:09 -0500

    Initial Commit

---
+++ SuperHeropedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperHeropedia
+//
+//  Created by Taylor Wright-Sanson on 10/13/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
